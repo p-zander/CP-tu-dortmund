bed0ccf984636561a1fc3240bf27868276bca9bf
p-zander <philipp.zander@udo.edu>
2015-04-26 16:27:36 +0200

    started working on the problems 1 and 2

--- src/03/harmoszi.cpp
+++ src/03/harmoszi.cpp
@@ -1,8 +1,21 @@
+#include <Eigen/Core>
 #include <functional>
+#include <vector>
 #include <cmath>
+#include <iostream>
 
-int int main(int argc, char const *argv[])
-{
-    /* code */
+#include "rungekutta.h"
+
+using std::vector; using std::cout; using std::endl; using Eigen::Vector3d;
+
+int main() {
+    Vector3d r0(1,0,0);
+    Vector3d v0(0,0,0);
+
+    vector<VectorXd> r = RK4_newton(r0, v0, 0, 10, 100,[](Vector3d r, double t){ return -r; });
+
+    for(const auto& x : r) 
+        cout << x[0] << "\t" << x[1] << "\t" << x[2] << "\t" << endl;
+
     return 0;
 }
--- src/03/rungekutta.h
+++ src/03/rungekutta.h
@@ -1,6 +1,39 @@
+#include <Eigen/Core>
 #include <functional>
+#include <utility>
+#include <vector>
 #include <cmath>
 
-double RungeKutta4(double t0, double y0, double h, std::function<double(double, double)>) {
-    
+using Eigen::VectorXd; using std::pair; using std::vector;
+
+pair<VectorXd, VectorXd> RK4_newton(VectorXd rn, VectorXd vn, double tn, double h,
+                                    const std::function<VectorXd(VectorXd, double)>& f) {
+    assert(rn.cols() == vn.cols());
+
+    VectorXd k0 = h *   vn;
+    VectorXd l0 = h * f(rn,         tn);
+    VectorXd k1 = h *  (vn + .5*l0);
+    VectorXd l1 = h * f(rn + .5*k0, tn + .5*h);
+    VectorXd k2 = h *  (vn + .5*l1);
+    VectorXd l2 = h * f(rn + .5*k1, tn + .5*h);
+    VectorXd k3 = h *  (vn +    l2);
+    VectorXd l3 = h * f(rn +    k2, tn +    h);
+
+    return std::make_pair(rn + 1./6*(k0 + 2*k1 + 2*k2 + k3), vn + 1./6*(l0 + 2*l1 + 2*l2 + l3));
 }
+
+vector<VectorXd> RK4_newton(VectorXd r0, VectorXd v0, double t0, double tN, int N,
+                    const std::function<VectorXd(VectorXd, double)>& f) {
+    pair<VectorXd, VectorXd> val = std::make_pair(r0, v0);
+    vector<VectorXd> res {r0};
+
+    double h = (tN-t0)/N;
+
+    for (int i=1; i <=N; i++) {
+        val = RK4_newton(std::get<0>(val), std::get<1>(val), h*N, h, f);
+        res.push_back(std::get<0>(val));
+    }
+
+    return res;
+}
+
