e2586fb043f9759faa5d007e49a4e270c45b9889
p-zander <philipp.zander@udo.edu>
2015-04-26 12:24:10 +0200

    Started working on the new problems. initial

---
+++ src/03/harmoszi.cpp
@@ -0,0 +1,8 @@
+#include <functional>
+#include <cmath>
+
+int int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ src/03/rungekutta.h
@@ -0,0 +1,6 @@
+#include <functional>
+#include <cmath>
+
+double RungeKutta4(double t0, double y0, double h, std::function<double(double, double)>) {
+    
+}
