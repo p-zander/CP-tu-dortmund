3174230256770b642bca806c36d0b2b183ffcede
p-zander <philipp.zander@udo.edu>
2015-04-26 19:09:00 +0200

    Finished problems 1 and 2. Plots of r,v and energy are to be made.

--- src/03/harmoszi.cpp
+++ src/03/harmoszi.cpp
@@ -10,12 +10,25 @@ using std::vector; using std::cout; using std::endl; using Eigen::Vector3d;
 
 int main() {
     Vector3d r0(1,0,0);
-    Vector3d v0(0,0,0);
+    Vector3d v0(0,1,0);
 
-    vector<VectorXd> r = RK4_newton(r0, v0, 0, 10, 100,[](Vector3d r, double t){ return -r; });
+    vector<VectorXd> r;
+    vector<VectorXd> v;
 
-    for(const auto& x : r) 
-        cout << x[0] << "\t" << x[1] << "\t" << x[2] << "\t" << endl;
+    size_t steps = 100;
+    double t0 = 0;
+    double tN = 10;
+    double h  = (tN-t0)/steps;
+
+    std::tie(r, v) = RK4_newton(r0, v0, t0, tN, steps, [](Vector3d r, double t){ return -r; });
+
+    cout << "# t \t x \t y \t z \t v_x \t v_y \t v_z" << endl;
+
+    for (size_t i = 0; i <= steps; i++) { 
+        cout <<   i*h   << "\t"
+             << r[i][0] << "\t" << r[i][1] << "\t" << r[i][2] << "\t"
+             << v[i][0] << "\t" << v[i][1] << "\t" << v[i][2] << "\t" << endl;
+    }
 
     return 0;
 }
--- src/03/rungekutta.h
+++ src/03/rungekutta.h
@@ -6,9 +6,10 @@
 
 using Eigen::VectorXd; using std::pair; using std::vector;
 
-pair<VectorXd, VectorXd> RK4_newton(VectorXd rn, VectorXd vn, double tn, double h,
-                                    const std::function<VectorXd(VectorXd, double)>& f) {
-    assert(rn.cols() == vn.cols());
+pair<VectorXd, VectorXd>
+RK4_newton(VectorXd rn, VectorXd vn, double tn, double h,
+            const std::function<VectorXd(VectorXd, double)>& f) {
+    assert(rn.rows() == vn.rows());
 
     VectorXd k0 = h *   vn;
     VectorXd l0 = h * f(rn,         tn);
@@ -22,18 +23,21 @@ pair<VectorXd, VectorXd> RK4_newton(VectorXd rn, VectorXd vn, double tn, double 
     return std::make_pair(rn + 1./6*(k0 + 2*k1 + 2*k2 + k3), vn + 1./6*(l0 + 2*l1 + 2*l2 + l3));
 }
 
-vector<VectorXd> RK4_newton(VectorXd r0, VectorXd v0, double t0, double tN, int N,
-                    const std::function<VectorXd(VectorXd, double)>& f) {
+pair<vector<VectorXd>, vector<VectorXd>>
+RK4_newton(VectorXd r0, VectorXd v0, double t0, double tN, int N,
+            const std::function<VectorXd(VectorXd, double)>& f) {
     pair<VectorXd, VectorXd> val = std::make_pair(r0, v0);
-    vector<VectorXd> res {r0};
+    vector<VectorXd> res_r {r0};
+    vector<VectorXd> res_v {v0};
 
     double h = (tN-t0)/N;
 
     for (int i=1; i <=N; i++) {
         val = RK4_newton(std::get<0>(val), std::get<1>(val), h*N, h, f);
-        res.push_back(std::get<0>(val));
+        res_r.push_back(std::get<0>(val));
+        res_v.push_back(std::get<1>(val));
     }
 
-    return res;
+    return std::make_pair(res_r, res_v);
 }
 
