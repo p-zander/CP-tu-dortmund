22f305836912280b7d3c3f0ac3196a6079578f6d
p-zander <philipp.zander@udo.edu>
2015-04-29 14:01:33 +0200

    Made main generating two files to plot

--- src/03/harmoszi.cpp
+++ src/03/harmoszi.cpp
@@ -3,31 +3,53 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
-#include "rungekutta.h"
+#include "./rungekutta.h"
 
-using std::vector; using std::cout; using std::endl; using Eigen::Vector3d;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using Eigen::Vector3d;
 
 int main() {
-    Vector3d r0(1,0,0);
-    Vector3d v0(0,1,0);
-
-    vector<VectorXd> r;
-    vector<VectorXd> v;
-
-    size_t steps = 100;
+    Vector3d r0(1, 0, 0);
     double t0 = 0;
-    double tN = 10;
-    double h  = (tN-t0)/steps;
+    double tN = 8*3.14159265358;
+    
+    Vector3d v0(0, 0, 0);
+    size_t steps = 24;  // h = 1.0472
+    double h = (tN - t0) / steps;
 
-    std::tie(r, v) = RK4_newton(r0, v0, t0, tN, steps, [](Vector3d r, double t){ return -r; });
+    vector<VectorXd> r, v;
 
-    cout << "# t \t x \t y \t z \t v_x \t v_y \t v_z" << endl;
+    std::tie(r, v) = RK4_newton(r0, v0, t0, tN, steps, [](Vector3d r, double) { return -r; });
 
-    for (size_t i = 0; i <= steps; i++) { 
-        cout <<   i*h   << "\t"
-             << r[i][0] << "\t" << r[i][1] << "\t" << r[i][2] << "\t"
-             << v[i][0] << "\t" << v[i][1] << "\t" << v[i][2] << "\t" << endl;
+    ofstream file1("2a.txt");
+    file1 << "# t \t x \t y \t z \t v_x \t v_y \t v_z" << endl;
+    for (size_t i = 0; i <= steps; i++) {
+        file1 <<   i*h   << "\t"
+              << r[i][0] << "\t" << r[i][1] << "\t" << r[i][2] << "\t"
+              << v[i][0] << "\t" << v[i][1] << "\t" << v[i][2] << "\t" << endl;
+    }
+
+    v0 = Vector3d(0, 1, 0);
+    steps = 88;  // h = 0.2856
+    h = (tN - t0) / steps;
+
+    r.clear();
+    v.clear();
+    std::tie(r, v) = RK4_newton(r0, v0, t0, tN, steps, [](Vector3d r, double) { return -r; });
+
+    ofstream file2("2b.txt");
+
+    file2 << "# t \t x \t y \t z \t v_x \t v_y \t v_z" << endl;
+
+    for (size_t i = 0; i <= steps; i++) {
+        file2 <<   i*h   << "\t"
+              << r[i][0] << "\t" << r[i][1] << "\t" << r[i][2] << "\t"
+              << v[i][0] << "\t" << v[i][1] << "\t" << v[i][2] << "\t" << endl;
     }
 
     return 0;
