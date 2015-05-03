6e8adcfecdc1283b384f02ac2034c6d16d3a1900
hassec <hasse.christoph@outlook.de>
2015-05-03 15:03:58 +0200

    still in progress

--- src/04/zeeman.cpp
+++ src/04/zeeman.cpp
@@ -1,16 +1,23 @@
+#include <Eigen/Core>
+#include "rungekutta.h"
+#include <utility>
 #include <iostream>
 #include <cmath>
 #include <functional>
+#include <vector>
 
+using Eigen::Vector3d;
+using Eigen::VectorXd;
 using std::cout;
 using std::endl;
+using std::vector;
 
 const double R = 0.05;     // m
 const double B = 0.1;      // m
 const double A = 0.2;      // m
 const double K = 0.25;     // Kg/s²
-// const double I = 6250e-7;  // Kg*m²
-// const double G = 2e-4;     // Kg*m²/s
+const double I = 6250e-7;  // Kg*m²
+const double G = 2e-4;     // Kg*m²/s
 
 double V(double theta, double x, double y) {
   double d1 = sqrt(A * A + 2 * A * R * cos(theta) + R * R);
@@ -32,10 +39,26 @@ double deriveV(double theta, double x, double y, double h = 1e-3) {
 }
 
 
+
 int main() { 
 
 	const double x = 0.18;
 	const double y = 0.01;
+
+	VectorXd r0(1);
+	r0[0]=1;
+
+    double t0 = 0;
+    double tN = 4*M_PI;
+
+    VectorXd w0(1);
+    w0[0]=1;
+    size_t steps = 12;  // h = 1.0472
+    double h = (tN - t0) / steps;
+
+    vector<VectorXd> r, v;
+
+	std::tie(r, v) = RK4_newton(r0, w0, t0, tN, steps, [x,y](VectorXd theta, VectorXd w, double) { return 1/I*(-G*w[0]+F(theta[0],x,y)); });
 
 	for (int i = 0; i < 360 ; ++i)
 	{
