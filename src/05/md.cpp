fcf1d3c94eb4418b8591cb8802c43c3a46c7183c
p-zander <philipp.zander@udo.edu>
2015-05-10 21:28:13 +0200

    Finished initialization of the simulation

--- src/05/md.cpp
+++ src/05/md.cpp
@@ -15,70 +15,85 @@
 
 #include "../Utils/utils.h"
 
-using std::pair; using std::function; using std::vector; using std::cout; using std::endl; using std::make_pair;
+using std::pair;
+using std::function;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::make_pair;
+using std::tie;
 using Eigen::Vector2d;
 
-pair<Vector2d, Vector2d> Verlet_step(Vector2d x_0, Vector2d v_0, double m,
-        double t_0, double h, const function<Vector2d(Vector2d, double)>& F) {
-    Vector2d x_1 = x_0 + h * v_0 + h*h/(2*m) * F(x_0, t_0);
-    Vector2d v_1 = v_0 + h/(2*m) * (F(x_0, t_0) + F(x_1, t_0+h));
+double K_B = 1.3806488e-23;
+
+pair<Vector2d, Vector2d> Verlet_step(Vector2d x_0, Vector2d v_0, double m, double t_0, double h,
+                                     const function<Vector2d(Vector2d, double)> &F) {
+    Vector2d x_1 = x_0 + h * v_0 + h * h / (2 * m) * F(x_0, t_0);
+    Vector2d v_1 = v_0 + h / (2 * m) * (F(x_0, t_0) + F(x_1, t_0 + h));
     return std::make_pair(x_1, v_1);
 }
 
 // vector<pair<Vector2d, Vector2d>>
 // Verlet(vector<Vector2d> x_0, vector<Vector2d> v_0, double m, double t_0,
-//         double t_N, double h, const function<Vector2d(Vector2d, double)>& F) {
+//         double t_N, double h, const function<Vector2d(Vector2d, double)>& F)
+//         {
 //     for (int i = 0; i <= t_N; i++) {
 //     }
-
 // }
 
-void initialize(vector<Vector2d>& loc, vector<Vector2d>& vel, double L, double T_0) {
+pair<vector<Vector2d>, vector<Vector2d>> initialize(double L, double T_0, double m) {
     namespace rand = boost::random;
 
-    rand::mt19937 generator;  // Mersenne Twister Generator
-    rand::uniform_real_distribution<> normdist(-5.0, 5.0);  // Distribution
-    rand::variate_generator<rand::mt19937&, rand::uniform_real_distribution<>> norm_rnd(generator, normdist);
+    rand::mt19937 generator;                                // Mersenne Twister Generator
+    rand::uniform_real_distribution<> normdist(-1.0, 1.0);  // Distribution
+    rand::variate_generator<rand::mt19937 &, rand::uniform_real_distribution<>> norm_rnd(generator, normdist);
     // Combination of distribution and generator
 
-    Vector2d v_mittel;
+    vector<Vector2d> loc, vel;
+    Vector2d v_mean(0, 0);
+    double v_squared_mean = 0;
     int i = 0;
-    for (int n = 0; n < 4; ++n)
-    {
-        for (int m = 0; m < 4; ++m)
-        {
-            loc.push_back(L/8 * Vector2d(1 + 2*n, 1 + 2*m));
+
+    for (int n = 0; n < 4; ++n) {
+        for (int m = 0; m < 4; ++m) {
+            loc.push_back(L / 8 * Vector2d(1 + 2 * n, 1 + 2 * m));
+
             Vector2d v_tmp = Vector2d(norm_rnd(), norm_rnd());
             vel.push_back(v_tmp);
-            v_mittel += v_tmp;
+
+            v_mean += v_tmp;
+            v_squared_mean += v_tmp.squaredNorm();
             i++;
         }
     }
-    v_mittel /= i;
+    v_mean /= i;
+    v_squared_mean /= i;
 
-    for (Vector2d& v : vel) {
-        v = v - v_mittel;
-    }
-    // TODO(p-zander): scale velocities to match temperature T_0
+    double scale = 3 * K_B * T_0 / m;
+
+    for (Vector2d &v : vel) v = (v - v_mean) * sqrt(scale / v_squared_mean);
+    return make_pair(loc, vel);
 }
 
-Vector2d F(const Vector2d& x, double t) {
-    return 24 * (pow(x.squaredNorm(), 3) - 2)/pow(x.squaredNorm(), 7) * x;
-}
+Vector2d F(const Vector2d &x, double t) { return 24 * (pow(x.squaredNorm(), 3) - 2) / pow(x.squaredNorm(), 7) * x; }
 
 // double F_norm(const Vector2d& x, double t) {
-//     return -24./x.norm() * (2./pow(x.squaredNorm(), 6) - 1./pow(x.squaredNorm(), 3));
+//     return -24./x.norm() * (2./pow(x.squaredNorm(), 6) -
+//     1./pow(x.squaredNorm(), 3));
 // }
 
-Vector2d F(const vector<Vector2d>& particles, const Vector2d& x, double t,
-            double L, double cutoff = -1) {
-    if (cutoff <= 0) cutoff = L/2;
+Vector2d F(const vector<Vector2d> &particles, const Vector2d &x, double t, double L, double cutoff = -1) {
+    if (cutoff <= 0) cutoff = L / 2;
 
     Vector2d F_eff(0, 0);
-    for (const Vector2d& y: particles) {
-        if (x == y || (x-y).norm() > cutoff) continue;
+    for (const Vector2d &y : particles) {
+        if (x == y) continue;
         for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) F_eff += F(x-(y+Vector2d(i*L, j*L)), t);
+            for (int j = -1; j < 2; j++) {
+                Vector2d y_new = y + Vector2d(i * L, j * L);
+                if ((x - y_new).norm() > cutoff) continue;
+                F_eff += F(x - y_new, t);
+            }
         }
     }
     return F_eff;
@@ -86,19 +101,25 @@ Vector2d F(const vector<Vector2d>& particles, const Vector2d& x, double t,
 
 int main(int argc, char const *argv[]) {
     int L = 8;
+    double cutoff = L/2;
+    double m = 1;
     double T_0 = 1;
     vector<Vector2d> locations;
     vector<Vector2d> velocities;
 
-    initialize(locations, velocities, L, T_0);
+    tie(locations, velocities) = initialize(L, T_0, m);
 
-    vector<double> x, y, v_x, v_y;
+    vector<double> x, y, v_x, v_y, F_x, F_y;
 
-    for (const Vector2d& r: locations) {
+    for (const Vector2d &r : locations) {
         x.push_back(r[0]);
         y.push_back(r[1]);
+
+        Vector2d force = F(locations, r, 1, L, cutoff);
+        F_x.push_back(force[0]);
+        F_y.push_back(force[1]);
     }
-    for (const Vector2d& v: velocities) {
+    for (const Vector2d &v : velocities) {
         v_x.push_back(v[0]);
         v_y.push_back(v[1]);
     }
@@ -114,16 +135,19 @@ int main(int argc, char const *argv[]) {
                  "import numpy as np \n"
                  "print 'importing matplotlib...' \n"
                  "from matplotlib import pyplot as plt \n"
-                 "print 'done' \n", global, global);
+                 "print 'done' \n",
+                 global, global);
 
         global["L"] = L;
+        global["cutoff"] = cutoff;
 
         utils::to_numpy(make_pair(x, y), make_pair("x", "y"), &global, &global);
         utils::to_numpy(make_pair(v_x, v_y), make_pair("v_x", "v_y"), &global, &global);
+        utils::to_numpy(make_pair(F_x, F_y), make_pair("F_x", "F_y"), &global, &global);
 
         // Launch some function in Python.
         py::exec_file("plots.py", global, global);
-    }catch(const py::error_already_set&) {
+    } catch (const py::error_already_set &) {
         cout << utils::extractPythonException() << endl;
         exit(EXIT_FAILURE);
     }
--- src/05/plots.py
+++ src/05/plots.py
@@ -1,19 +1,27 @@
 
 # coding: utf-8
+from matplotlib import pyplot as plt
 
+plt.figure(figsize=[8, 8])
 plt.plot(x, y, 'bo')
-plt.quiver(x, y, v_x, v_y, pivot='middle', linestyle='dashed')
+circle = plt.Circle((x[0], y[0]), cutoff, color='k', fill=False, linestyle='dashed')
+plt.quiver(x, y, v_x, v_y, pivot='middle', headlength=2, headaxislength=1)
+plt.quiver(x, y, F_x, F_y, color='red', pivot='middle',
+           headlength=2, headaxislength=1)
 
-plt.xlim(-L, 2*L)
-plt.ylim(-L, 2*L)
+plt.xlim(-L, 2 * L)
+plt.ylim(-L, 2 * L)
 
-plt.hlines([0, L], -L, 2*L, linestyles='dashed')
-plt.vlines([0, L], -L, 2*L, linestyles='dashed')
+plt.hlines([0, L], -L, 2 * L, linestyles='dashed')
+plt.vlines([0, L], -L, 2 * L, linestyles='dashed')
 
-for m in [-L,0,L]:
-    for n in [-L,0,L]:
-        if [n,m] is not [0,0]:
-            plt.plot(x+n, y+m, 'bo', alpha=0.5)
-            plt.quiver(x+n, y+m, v_x, v_y, pivot='middle', alpha=0.5, linestyle='dashed')
+for m in [-L, 0, L]:
+    for n in [-L, 0, L]:
+        if n is not 0 or m is not 0:
+            plt.plot(x + n, y + m, 'bo', alpha=0.5)
+            plt.quiver(x + n, y + m, v_x, v_y, pivot='middle',
+                       alpha=0.5, headlength=2, headaxislength=1)
+
+plt.gca().add_artist(circle)
 
 plt.savefig('init.pdf')
