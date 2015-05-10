80eeaaac2c08cab713cb080fab297386f9315613
p-zander <philipp.zander@udo.edu>
2015-05-10 22:27:48 +0200

    Started working on the Verlet algorithm. Doesn't seem to do the right thing

--- src/05/md.cpp
+++ src/05/md.cpp
@@ -26,17 +26,55 @@ using Eigen::Vector2d;
 
 double K_B = 1.3806488e-23;
 
-pair<Vector2d, Vector2d> Verlet_step(Vector2d x_0, Vector2d v_0, double m, double t_0, double h,
-                                     const function<Vector2d(Vector2d, double)> &F) {
+Vector2d F(const Vector2d &x, double t) { return 24 * (pow(x.squaredNorm(), 3) - 2) / pow(x.squaredNorm(), 7) * x; }
+
+// double F_norm(const Vector2d& x, double t) {
+//     return -24./x.norm() * (2./pow(x.squaredNorm(), 6) -
+//     1./pow(x.squaredNorm(), 3));
+// }
+
+Vector2d F(const vector<Vector2d> &particles, const Vector2d &x, double t, double L, double cutoff = -1) {
+    if (cutoff <= 0) cutoff = L / 2;
+
+    Vector2d F_eff(0, 0);
+    for (const Vector2d &y : particles) {
+        if (x == y) continue;
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                Vector2d y_new = y + Vector2d(i * L, j * L);
+                if ((x - y_new).norm() > cutoff) continue;
+                F_eff += F(x - y_new, t);
+            }
+        }
+    }
+    return F_eff;
+}
+
+pair<Vector2d, Vector2d> Verlet_step_one(Vector2d x_0, Vector2d v_0, double m, double t_0, double h,
+                                         const function<Vector2d(Vector2d, double)> &F) {
     Vector2d x_1 = x_0 + h * v_0 + h * h / (2 * m) * F(x_0, t_0);
     Vector2d v_1 = v_0 + h / (2 * m) * (F(x_0, t_0) + F(x_1, t_0 + h));
     return std::make_pair(x_1, v_1);
 }
 
+pair<vector<Vector2d>, vector<Vector2d>>
+Verlet_step(const vector<Vector2d> &loc_0, const vector<Vector2d> &vel_0, double m, double t_0, double h,
+            const function<Vector2d(vector<Vector2d>, Vector2d x, double)> &F) {
+    assert(loc_0.size() == vel_0.size());
+    vector<Vector2d> loc_1, vel_1;
+
+    for (int i = 0; i < loc_0.size(); ++i) {
+        Vector2d x, v;
+        tie(x, v) = Verlet_step_one(loc_0[i], vel_0[i], m, t_0, h,
+                                    [&loc_0, &F](Vector2d x, double t) { return F(loc_0, x, t); });
+        loc_1.push_back(x);
+        vel_1.push_back(v);
+    }
+    return make_pair(loc_1, vel_1);
+}
 // vector<pair<Vector2d, Vector2d>>
 // Verlet(vector<Vector2d> x_0, vector<Vector2d> v_0, double m, double t_0,
-//         double t_N, double h, const function<Vector2d(Vector2d, double)>& F)
-//         {
+//         double t_N, double h, const function<Vector2d(Vector2d, double)>& F) {
 //     for (int i = 0; i <= t_N; i++) {
 //     }
 // }
@@ -69,59 +107,56 @@ pair<vector<Vector2d>, vector<Vector2d>> initialize(double L, double T_0, double
     v_mean /= i;
     v_squared_mean /= i;
 
-    double scale = 3 * K_B * T_0 / m;
+    double scale = 10;  // 3 * K_B * T_0 / m;
 
     for (Vector2d &v : vel) v = (v - v_mean) * sqrt(scale / v_squared_mean);
     return make_pair(loc, vel);
 }
 
-Vector2d F(const Vector2d &x, double t) { return 24 * (pow(x.squaredNorm(), 3) - 2) / pow(x.squaredNorm(), 7) * x; }
-
-// double F_norm(const Vector2d& x, double t) {
-//     return -24./x.norm() * (2./pow(x.squaredNorm(), 6) -
-//     1./pow(x.squaredNorm(), 3));
-// }
-
-Vector2d F(const vector<Vector2d> &particles, const Vector2d &x, double t, double L, double cutoff = -1) {
-    if (cutoff <= 0) cutoff = L / 2;
-
-    Vector2d F_eff(0, 0);
-    for (const Vector2d &y : particles) {
-        if (x == y) continue;
-        for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) {
-                Vector2d y_new = y + Vector2d(i * L, j * L);
-                if ((x - y_new).norm() > cutoff) continue;
-                F_eff += F(x - y_new, t);
-            }
-        }
-    }
-    return F_eff;
-}
-
 int main(int argc, char const *argv[]) {
     int L = 8;
-    double cutoff = L/2;
+    double cutoff = L / 2;
     double m = 1;
-    double T_0 = 1;
-    vector<Vector2d> locations;
-    vector<Vector2d> velocities;
 
-    tie(locations, velocities) = initialize(L, T_0, m);
+    double h = 0.01;
+    // double t_max = 10;
 
-    vector<double> x, y, v_x, v_y, F_x, F_y;
+    vector<Vector2d> locations_0, locations_1;
+    vector<Vector2d> velocities_0, velocities_1;
 
-    for (const Vector2d &r : locations) {
-        x.push_back(r[0]);
-        y.push_back(r[1]);
+    tie(locations_0, velocities_0) = initialize(L, 1, m);
 
-        Vector2d force = F(locations, r, 1, L, cutoff);
-        F_x.push_back(force[0]);
-        F_y.push_back(force[1]);
+    tie(locations_1, velocities_1) =
+        Verlet_step(locations_0, velocities_0, m, 0, h, [L, cutoff](const vector<Vector2d> &loc, const Vector2d &x,
+                                                                    double t) { return F(loc, x, t, L, cutoff); });
+
+    // prepare for plotting
+    vector<double> x_0, y_0, v_x_0, v_y_0, F_x_0, F_y_0;
+    vector<double> x_1, y_1, v_x_1, v_y_1, F_x_1, F_y_1;
+
+    for (const Vector2d &r : locations_0) {
+        x_0.push_back(r[0]);
+        y_0.push_back(r[1]);
+
+        Vector2d force = F(locations_0, r, 1, L, cutoff);
+        F_x_0.push_back(force[0]);  // to test if the force is zero in the beginning
+        F_y_0.push_back(force[1]);
     }
-    for (const Vector2d &v : velocities) {
-        v_x.push_back(v[0]);
-        v_y.push_back(v[1]);
+    for (const Vector2d &v : velocities_0) {
+        v_x_0.push_back(v[0]);
+        v_y_0.push_back(v[1]);
+    }
+    for (const Vector2d &r : locations_1) {
+        x_1.push_back(r[0]);
+        y_1.push_back(r[1]);
+
+        Vector2d force = F(locations_1, r, 1, L, cutoff);
+        F_x_1.push_back(force[0]);
+        F_y_1.push_back(force[1]);
+    }
+    for (const Vector2d &v : velocities_1) {
+        v_x_1.push_back(v[0]);
+        v_y_1.push_back(v[1]);
     }
 
     namespace py = boost::python;
@@ -131,6 +166,7 @@ int main(int argc, char const *argv[]) {
 
         // Retrieve the main module's namespace
         py::object global(py::import("__main__").attr("__dict__"));
+
         py::exec("print 'Hello from Python!' \n"
                  "import numpy as np \n"
                  "print 'importing matplotlib...' \n"
@@ -141,9 +177,12 @@ int main(int argc, char const *argv[]) {
         global["L"] = L;
         global["cutoff"] = cutoff;
 
-        utils::to_numpy(make_pair(x, y), make_pair("x", "y"), &global, &global);
-        utils::to_numpy(make_pair(v_x, v_y), make_pair("v_x", "v_y"), &global, &global);
-        utils::to_numpy(make_pair(F_x, F_y), make_pair("F_x", "F_y"), &global, &global);
+        utils::to_numpy(make_pair(x_0, y_0), make_pair("x_0", "y_0"), &global, &global);
+        utils::to_numpy(make_pair(v_x_0, v_y_0), make_pair("v_x_0", "v_y_0"), &global, &global);
+        utils::to_numpy(make_pair(F_x_0, F_y_0), make_pair("F_x_0", "F_y_0"), &global, &global);
+        utils::to_numpy(make_pair(x_1, y_1), make_pair("x_1", "y_1"), &global, &global);
+        utils::to_numpy(make_pair(v_x_1, v_y_1), make_pair("v_x_1", "v_y_1"), &global, &global);
+        utils::to_numpy(make_pair(F_x_1, F_y_1), make_pair("F_x_1", "F_y_1"), &global, &global);
 
         // Launch some function in Python.
         py::exec_file("plots.py", global, global);
--- src/05/plots.py
+++ src/05/plots.py
@@ -3,10 +3,10 @@
 from matplotlib import pyplot as plt
 
 plt.figure(figsize=[8, 8])
-plt.plot(x, y, 'bo')
-circle = plt.Circle((x[0], y[0]), cutoff, color='k', fill=False, linestyle='dashed')
-plt.quiver(x, y, v_x, v_y, pivot='middle', headlength=2, headaxislength=1)
-plt.quiver(x, y, F_x, F_y, color='red', pivot='middle',
+plt.plot(x_0, y_0, 'bo')
+circle = plt.Circle((x_0[0], y_0[0]), cutoff, color='k', fill=False, linestyle='dashed')
+plt.quiver(x_0, y_0, v_x_0, v_y_0, pivot='middle', headlength=2, headaxislength=1)
+plt.quiver(x_0, y_0, F_x_0, F_y_0, color='red', pivot='middle',
            headlength=2, headaxislength=1)
 
 plt.xlim(-L, 2 * L)
@@ -18,10 +18,33 @@ plt.vlines([0, L], -L, 2 * L, linestyles='dashed')
 for m in [-L, 0, L]:
     for n in [-L, 0, L]:
         if n is not 0 or m is not 0:
-            plt.plot(x + n, y + m, 'bo', alpha=0.5)
-            plt.quiver(x + n, y + m, v_x, v_y, pivot='middle',
+            plt.plot(x_0 + n, y_0 + m, 'bo', alpha=0.5)
+            plt.quiver(x_0 + n, y_0 + m, v_x_0, v_y_0, pivot='middle',
                        alpha=0.5, headlength=2, headaxislength=1)
 
 plt.gca().add_artist(circle)
 
 plt.savefig('init.pdf')
+
+plt.figure(figsize=[8, 8])
+plt.plot(x_0, y_0, 'bo')
+plt.plot(x_1, y_1, 'bo')
+plt.quiver(x_1, y_1, v_x_1, v_y_1, pivot='middle', headlength=2, headaxislength=1)
+plt.quiver(x_1, y_1, F_x_1, F_y_1, color='red', pivot='middle',
+           headlength=2, headaxislength=1)
+
+plt.xlim(0, L)
+plt.ylim(0, L)
+
+# plt.hlines([0, L], -L, 2 * L, linestyles='dashed')
+# plt.vlines([0, L], -L, 2 * L, linestyles='dashed')
+
+# for m in [-L, 0, L]:
+#     for n in [-L, 0, L]:
+#         if n is not 0 or m is not 0:
+#             plt.plot(x_1 + n, y_1 + m, 'bo', alpha=0.5)
+#             plt.quiver(x_1 + n, y_1 + m, v_x_1, v_y_1, pivot='middle',
+#                        alpha=0.5, headlength=2, headaxislength=1)
+
+
+plt.savefig('first_step.pdf')
