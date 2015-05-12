9c081e2236b8c1bea727978791d9850537d01ed9
p-zander <philipp.zander@udo.edu>
2015-05-13 00:40:49 +0200

    Changed the plotting script an the program to interact through the new to_numpy function for Eigen vectors. Add a constant scale factor for quiver to make the different plots comparable. Changed the initialization to take dynamic number of particles and box length. Small changes to the Makefile. Verlet_step may be doing the right thing...

--- src/05/md.cpp
+++ src/05/md.cpp
@@ -4,14 +4,10 @@
 
 #include <Python.h>
 
-#include <sstream>
 #include <iostream>
 #include <utility>
-#include <cmath>
 #include <functional>
-#include <algorithm>
 #include <vector>
-#include <string>
 
 #include "../Utils/utils.h"
 
@@ -22,20 +18,19 @@ using std::cout;
 using std::endl;
 using std::make_pair;
 using std::tie;
+using std::bind;
 using Eigen::Vector2d;
 
-double K_B = 1.3806488e-23;
+const double M = 1;
+const double L = 10;
+const unsigned int N = static_cast<unsigned int>(pow(3, 2));
 
-Vector2d F(const Vector2d &x, double t) { return 24 * (pow(x.squaredNorm(), 3) - 2) / pow(x.squaredNorm(), 7) * x; }
+inline Vector2d F_a_one(const Vector2d &x, double t) {
+    // return 24 * (pow(x.squaredNorm(), 3) - 2) / pow(x.squaredNorm(), 7) * x;
+    return 48 * x / x.squaredNorm() * (1. / pow(x.squaredNorm(), 6) - 1. / 2 * 1. / pow(x.squaredNorm(), 3));
+}
 
-// double F_norm(const Vector2d& x, double t) {
-//     return -24./x.norm() * (2./pow(x.squaredNorm(), 6) -
-//     1./pow(x.squaredNorm(), 3));
-// }
-
-Vector2d F(const vector<Vector2d> &particles, const Vector2d &x, double t, double L, double cutoff = -1) {
-    if (cutoff <= 0) cutoff = L / 2;
-
+Vector2d F_a(const vector<Vector2d> &particles, const Vector2d &x, double t, double cutoff) {
     Vector2d F_eff(0, 0);
     for (const Vector2d &y : particles) {
         if (x == y) continue;
@@ -43,43 +38,54 @@ Vector2d F(const vector<Vector2d> &particles, const Vector2d &x, double t, doubl
             for (int j = -1; j < 2; j++) {
                 Vector2d y_new = y + Vector2d(i * L, j * L);
                 if ((x - y_new).norm() > cutoff) continue;
-                F_eff += F(x - y_new, t);
+                F_eff += F_a_one(y_new - x, t);
             }
         }
     }
     return F_eff;
 }
 
-pair<Vector2d, Vector2d> Verlet_step_one(Vector2d x_0, Vector2d v_0, double m, double t_0, double h,
+pair<Vector2d, Vector2d> Verlet_step_one(Vector2d x_0, Vector2d v_0, double t_0, double h,
                                          const function<Vector2d(Vector2d, double)> &F) {
-    Vector2d x_1 = x_0 + h * v_0 + h * h / (2 * m) * F(x_0, t_0);
-    Vector2d v_1 = v_0 + h / (2 * m) * (F(x_0, t_0) + F(x_1, t_0 + h));
+    Vector2d x_1 = x_0 + h * v_0 + h * h / (2 * M) * F(x_0, t_0);
+    Vector2d v_1 = v_0 + h / (2 * M) * (F(x_0, t_0) + F(x_1, t_0 + h));
     return std::make_pair(x_1, v_1);
 }
 
-pair<vector<Vector2d>, vector<Vector2d>>
-Verlet_step(const vector<Vector2d> &loc_0, const vector<Vector2d> &vel_0, double m, double t_0, double h,
-            const function<Vector2d(vector<Vector2d>, Vector2d x, double)> &F) {
-    assert(loc_0.size() == vel_0.size());
+pair<vector<Vector2d>, vector<Vector2d>> Verlet_step(const vector<Vector2d> &loc_0, const vector<Vector2d> &vel_0,
+                                                     double t_0, double h,
+                                                     const function<Vector2d(vector<Vector2d>, Vector2d, double)> &F) {
+    if (loc_0.size() != vel_0.size()) {
+        std::cerr << "Verlet_step: Vectors loc_0 and vel_0 should have equal size! EXIT" << endl;
+        exit(EXIT_FAILURE);
+    }
     vector<Vector2d> loc_1, vel_1;
 
-    for (int i = 0; i < loc_0.size(); ++i) {
+    for (size_t i = 0; i < loc_0.size(); ++i) {
         Vector2d x, v;
-        tie(x, v) = Verlet_step_one(loc_0[i], vel_0[i], m, t_0, h,
-                                    [&loc_0, &F](Vector2d x, double t) { return F(loc_0, x, t); });
+        tie(x, v) = Verlet_step_one(loc_0[i], vel_0[i], t_0, h, bind(F, loc_0, _1, _2));
         loc_1.push_back(x);
         vel_1.push_back(v);
     }
     return make_pair(loc_1, vel_1);
 }
+
 // vector<pair<Vector2d, Vector2d>>
-// Verlet(vector<Vector2d> x_0, vector<Vector2d> v_0, double m, double t_0,
+// Verlet(vector<Vector2d> x_0, vector<Vector2d> v_0, double t_0,
 //         double t_N, double h, const function<Vector2d(Vector2d, double)>& F) {
 //     for (int i = 0; i <= t_N; i++) {
 //     }
 // }
 
-pair<vector<Vector2d>, vector<Vector2d>> initialize(double L, double T_0, double m) {
+pair<vector<Vector2d>, vector<Vector2d>> initialize(double T_0) {
+    int ppa = static_cast<int>(sqrt(N));
+    double dist = L / (2 * ppa);
+
+    if (pow(ppa, 2) != N) {
+        std::cerr << "initialize: sqrt(N) should be an integer! EXIT" << endl;
+        exit(EXIT_FAILURE);
+    }
+
     namespace rand = boost::random;
 
     rand::mt19937 generator;                                // Mersenne Twister Generator
@@ -90,74 +96,53 @@ pair<vector<Vector2d>, vector<Vector2d>> initialize(double L, double T_0, double
     vector<Vector2d> loc, vel;
     Vector2d v_mean(0, 0);
     double v_squared_mean = 0;
-    int i = 0;
 
-    for (int n = 0; n < 4; ++n) {
-        for (int m = 0; m < 4; ++m) {
-            loc.push_back(L / 8 * Vector2d(1 + 2 * n, 1 + 2 * m));
+    for (int k = 0; k < ppa; ++k) {
+        for (int l = 0; l < ppa; ++l) {
+            loc.push_back(Vector2d(dist * (1 + 2 * k), dist * (1 + 2 * l)));
 
             Vector2d v_tmp = Vector2d(norm_rnd(), norm_rnd());
             vel.push_back(v_tmp);
 
             v_mean += v_tmp;
             v_squared_mean += v_tmp.squaredNorm();
-            i++;
         }
     }
-    v_mean /= i;
-    v_squared_mean /= i;
+    v_mean /= N;
+    v_squared_mean /= N;
 
-    double scale = 10;  // 3 * K_B * T_0 / m;
-
+    double scale = 2 * T_0 / M;
     for (Vector2d &v : vel) v = (v - v_mean) * sqrt(scale / v_squared_mean);
+
+    v_squared_mean = 0;
+    for (const Vector2d &v : vel) v_squared_mean += v.squaredNorm();
+    v_squared_mean /= N;
+
+    cout << "temperature: " << .5 * M *v_squared_mean << endl;
+
     return make_pair(loc, vel);
 }
 
 int main(int argc, char const *argv[]) {
-    int L = 8;
-    double cutoff = L / 2;
-    double m = 1;
-
+    double cutoff = L / 2 + 1;
     double h = 0.01;
+
+    double t_0 = 0;
     // double t_max = 10;
 
     vector<Vector2d> locations_0, locations_1;
     vector<Vector2d> velocities_0, velocities_1;
 
-    tie(locations_0, velocities_0) = initialize(L, 1, m);
-
-    tie(locations_1, velocities_1) =
-        Verlet_step(locations_0, velocities_0, m, 0, h, [L, cutoff](const vector<Vector2d> &loc, const Vector2d &x,
-                                                                    double t) { return F(loc, x, t, L, cutoff); });
+    tie(locations_0, velocities_0) = initialize(0.5 /*= T_0*/);
+    tie(locations_1, velocities_1) = Verlet_step(locations_0, velocities_0, h, h, bind(F_a, _1, _2, _3, cutoff));
 
     // prepare for plotting
-    vector<double> x_0, y_0, v_x_0, v_y_0, F_x_0, F_y_0;
-    vector<double> x_1, y_1, v_x_1, v_y_1, F_x_1, F_y_1;
+    vector<Vector2d> F_0, F_1;
 
-    for (const Vector2d &r : locations_0) {
-        x_0.push_back(r[0]);
-        y_0.push_back(r[1]);
-
-        Vector2d force = F(locations_0, r, 1, L, cutoff);
-        F_x_0.push_back(force[0]);  // to test if the force is zero in the beginning
-        F_y_0.push_back(force[1]);
-    }
-    for (const Vector2d &v : velocities_0) {
-        v_x_0.push_back(v[0]);
-        v_y_0.push_back(v[1]);
-    }
-    for (const Vector2d &r : locations_1) {
-        x_1.push_back(r[0]);
-        y_1.push_back(r[1]);
-
-        Vector2d force = F(locations_1, r, 1, L, cutoff);
-        F_x_1.push_back(force[0]);
-        F_y_1.push_back(force[1]);
-    }
-    for (const Vector2d &v : velocities_1) {
-        v_x_1.push_back(v[0]);
-        v_y_1.push_back(v[1]);
-    }
+    for (const Vector2d &r : locations_0)
+        F_0.push_back(F_a(locations_0, r, t_0, cutoff)); // to test if the force is zero in the beginning
+    for (const Vector2d &r : locations_1)
+        F_1.push_back(F_a(locations_1, r, t_0, cutoff));
 
     namespace py = boost::python;
 
@@ -176,13 +161,15 @@ int main(int argc, char const *argv[]) {
 
         global["L"] = L;
         global["cutoff"] = cutoff;
+        global["velocity_scale"] = 12;
+        global["force_scale"] = 1e-3;
 
-        utils::to_numpy(make_pair(x_0, y_0), make_pair("x_0", "y_0"), &global, &global);
-        utils::to_numpy(make_pair(v_x_0, v_y_0), make_pair("v_x_0", "v_y_0"), &global, &global);
-        utils::to_numpy(make_pair(F_x_0, F_y_0), make_pair("F_x_0", "F_y_0"), &global, &global);
-        utils::to_numpy(make_pair(x_1, y_1), make_pair("x_1", "y_1"), &global, &global);
-        utils::to_numpy(make_pair(v_x_1, v_y_1), make_pair("v_x_1", "v_y_1"), &global, &global);
-        utils::to_numpy(make_pair(F_x_1, F_y_1), make_pair("F_x_1", "F_y_1"), &global, &global);
+        utils::to_numpy(locations_0, "r_0", &global, &global);
+        utils::to_numpy(velocities_0, "v_0", &global, &global);
+        utils::to_numpy(F_0, "F_0", &global, &global);
+        utils::to_numpy(locations_1, "r_1", &global, &global);
+        utils::to_numpy(velocities_1, "v_1", &global, &global);
+        utils::to_numpy(F_1, "F_1", &global, &global);
 
         // Launch some function in Python.
         py::exec_file("plots.py", global, global);
--- src/05/plots.py
+++ src/05/plots.py
@@ -3,11 +3,12 @@
 from matplotlib import pyplot as plt
 
 plt.figure(figsize=[8, 8])
-plt.plot(x_0, y_0, 'bo')
-circle = plt.Circle((x_0[0], y_0[0]), cutoff, color='k', fill=False, linestyle='dashed')
-plt.quiver(x_0, y_0, v_x_0, v_y_0, pivot='middle', headlength=2, headaxislength=1)
-plt.quiver(x_0, y_0, F_x_0, F_y_0, color='red', pivot='middle',
-           headlength=2, headaxislength=1)
+plt.plot(r_0[:,0], r_0[:,1], 'bo')
+circle = plt.Circle(r_0[0], cutoff, color='k', fill=False, linestyle='dashdot')
+if any(v_0[:,0]) or any(v_0[:,1]):
+    plt.quiver(r_0[:,0], r_0[:,1], v_0[:,0], v_0[:,1], scale=velocity_scale, pivot='middle', headlength=2, headaxislength=1)
+if any(F_0[:,0]) or any(F_0[:,1]):
+    plt.quiver(r_0[:,0], r_0[:,1], F_0[:,0], F_0[:,1], scale=force_scale, color='red', pivot='middle', headlength=2, headaxislength=1)
 
 plt.xlim(-L, 2 * L)
 plt.ylim(-L, 2 * L)
@@ -18,20 +19,21 @@ plt.vlines([0, L], -L, 2 * L, linestyles='dashed')
 for m in [-L, 0, L]:
     for n in [-L, 0, L]:
         if n is not 0 or m is not 0:
-            plt.plot(x_0 + n, y_0 + m, 'bo', alpha=0.5)
-            plt.quiver(x_0 + n, y_0 + m, v_x_0, v_y_0, pivot='middle',
-                       alpha=0.5, headlength=2, headaxislength=1)
+            plt.plot(r_0[:,0] + n, r_0[:,1] + m, 'bo', alpha=0.5)
+            if any(v_0[:,0]) or any(v_0[:,1]):
+                plt.quiver(r_0[:,0] + n, r_0[:,1] + m, v_0[:,0], v_0[:,1], scale=velocity_scale, pivot='middle', alpha=0.5, headlength=2, headaxislength=1)
 
 plt.gca().add_artist(circle)
 
 plt.savefig('init.pdf')
 
 plt.figure(figsize=[8, 8])
-plt.plot(x_0, y_0, 'bo')
-plt.plot(x_1, y_1, 'bo')
-plt.quiver(x_1, y_1, v_x_1, v_y_1, pivot='middle', headlength=2, headaxislength=1)
-plt.quiver(x_1, y_1, F_x_1, F_y_1, color='red', pivot='middle',
-           headlength=2, headaxislength=1)
+plt.plot(r_0[:,0], r_0[:,1], 'bo')
+plt.plot(r_1[:,0], r_1[:,1], 'bo')
+if any(v_1[:,0]) or any(v_1[:,1]):
+    plt.quiver(r_1[:,0], r_0[:,1], v_1[:,0], v_1[:,1], scale=velocity_scale, pivot='middle', headlength=2, headaxislength=1)
+if any(F_1[:,0]) or any(F_1[:,1]):
+    plt.quiver(r_1[:,0], r_1[:,1], F_1[:,0], F_1[:,1], scale=force_scale, color='red', pivot='middle', headlength=2, headaxislength=1)
 
 plt.xlim(0, L)
 plt.ylim(0, L)
@@ -42,8 +44,8 @@ plt.ylim(0, L)
 # for m in [-L, 0, L]:
 #     for n in [-L, 0, L]:
 #         if n is not 0 or m is not 0:
-#             plt.plot(x_1 + n, y_1 + m, 'bo', alpha=0.5)
-#             plt.quiver(x_1 + n, y_1 + m, v_x_1, v_y_1, pivot='middle',
+#             plt.plot(r_1[:,0] + n, r_1[:,1] + m, 'bo', alpha=0.5)
+#             plt.quiver(r_1[:,0] + n, r_1[:,1] + m, v_1[:,0], v_1[:,1], pivot='middle',
 #                        alpha=0.5, headlength=2, headaxislength=1)
 
 
