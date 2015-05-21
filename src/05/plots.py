13e0b3575fb4fc9c0ce869f93308a97e1f6640a6
hassec <hasse.christoph@outlook.de>
2015-05-21 19:26:41 +0200

    added file index to show used value for T-0, and set ylim for g(r)

--- src/05/plots.py
+++ src/05/plots.py
@@ -2,28 +2,30 @@
 
 #–– init locations and velocities ––––––––––––––––––––––––––––––––––––––––
 
-plt.plot(loc_sample[0,:, 0],   loc_sample[0,:, 1], 'bo') 
-plt.quiver(loc_sample[0,:, 0], loc_sample[0,:, 1], vel_sample[0,:, 0], vel_sample[0,:, 1], pivot='middle', headlength=2, headaxislength=1) 
+plt.plot(loc_sample[0, :, 0],   loc_sample[0,:, 1], 'bo') 
+plt.quiver(loc_sample[0, :, 0], loc_sample[0,:, 1], vel_sample[0,:, 0], vel_sample[0,:, 1], pivot='middle', headlength=2, headaxislength=1) 
 
 plt.xlabel(u'$x$ in $\sigma$')
 plt.ylabel(u'$y$ in $\sigma$')
 plt.xlim(0, L)
 plt.ylim(0, L)
 
-plt.savefig('init' + file_ext)
+plt.savefig('init' + str(T_0) + file_ext)
 
 #–– possibly animated locations –––––––––––––––––––––––––––––––––––––––––––––––
 # for i in range(N):
 #     plt.plot(loc_sample[:, i, 0], loc_sample[:, i, 1], ',')
-# plt.savefig('time' + file_ext)
+# plt.savefig('time' + str(T_0) + file_ext)
 
 # fig = plt.figure()
 # line, = plt.plot(loc_sample[0, :, 0], loc_sample[0,:, 1], 'bo')
 
+
 # def animate(i):
-# line.set_xdata(loc_sample[i, :, 0])  # update the data
+#     line.set_xdata(loc_sample[i, :, 0])  # update the data
 #     line.set_ydata(loc_sample[i, :, 1])
 #     return line,
+
 
 # def init():
 #     line.set_ydata(np.ma.array(loc_sample[0, :, 0], mask=True))
@@ -31,8 +33,9 @@ plt.savefig('init' + file_ext)
 #     plt.ylim(0, 8)
 #     return line,
 
-# ani = animation.FuncAnimation(fig, animate, np.arange(0,len(loc_sample),samples/100), init_func=init, interval=25)
-# ani.save("positions.gif", writer='imagemagick')
+# ani = animation.FuncAnimation(fig, animate, np.arange(
+#     0, 200, 1), init_func=init, interval=25)
+# ani.save("positions" + str(T_0) + ".gif", writer='imagemagick', fps=10)
 
 #–– Energy ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 plt.figure()
@@ -46,19 +49,19 @@ plt.plot(time, 1 / 2 * cm_vel_sample, label=u'$E_{kin,CM}$')
 plt.xlabel(u'$t$ in s')
 plt.ylabel(u'$E$ in $\epsilon$')
 
-plt.vlines(t_aqui, -30, 30)
+plt.vlines(t_aqui, -40, 40)
 
 plt.legend(loc='best')
 
-plt.savefig('energy' + file_ext)
+plt.savefig('energy' + str(T_0) + file_ext)
 
 #–– g(r) ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 plt.figure()
 
 plt.bar(np.arange(0, cutoff, cutoff / bins), radial_sample, cutoff / bins)
 plt.xlabel(u'$r$ in $\sigma$')
-
-plt.savefig('g_' + file_ext)
+plt.ylim(0,3)
+plt.savefig('g_' + str(T_0) + file_ext)
 
 #–– Temp ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 plt.figure()
@@ -69,7 +72,7 @@ plt.xlabel(u'$t$ in s')
 plt.ylabel(u'$T$ in $\\frac{\epsilon}{k_B}$')
 
 plt.legend(loc='best')
-plt.savefig('temperature' + file_ext)
+plt.savefig('temperature' + str(T_0) + file_ext)
 
 print 'mean temperature after equilibration was: ', np.mean(E_kin_sample[int(samples * t_aqui / t_max + 1):] / N)
 
@@ -80,4 +83,4 @@ print 'mean temperature after equilibration was: ', np.mean(E_kin_sample[int(sam
 # plt.figure()
 # plt.semilogx(T_init, T_mean)
 
-# plt.savefig('T_init_T_mean_curve' + file_ext)
+# plt.savefig('T_init_T_mean_curve' + str(T_0) + file_ext)
