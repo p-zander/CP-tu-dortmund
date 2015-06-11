8d3774ad42ee5243cbc92406ab48a09da2d0c499
p-zander <philipp.zander@udo.edu>
2015-06-11 02:04:24 +0200

    Add 3d scatterplot to prove correlation of 'random' numbers. Changed implementation of central limit theorem to an iterative one and got a huge performance boost. Furthermore the central limit is now the expected gaussian. Changed plots a little bit.

--- src/08/plots.py
+++ src/08/plots.py
@@ -1,47 +1,63 @@
 # coding: utf-8
 
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 plt.style.use('ggplot')
+plt.switch_backend('AGG')
 
 data = np.genfromtxt("lcg.txt")
 
-plt.figure()
+fig = plt.figure()
 plt.subplots_adjust(hspace=0.3)
-plt.subplot(221)
-plt.hist(data[:1e4, 0], normed=True, range=(0, 1))
-plt.title('LCG 1')
 
-plt.subplot(222)
-plt.hist(data[:1e4, 1], normed=True, range=(0, 1))
-plt.title('LCG 2')
+ax = fig.add_subplot(221, title='LCG1')
+plt.hist(data[:, 0], normed=True, range=(0, 1))
 
-plt.subplot(223)
+ax = fig.add_subplot(222, title='LCG2')
+plt.hist(data[:, 1], normed=True, range=(0, 1))
+
+ax = fig.add_subplot(223, title='LCG3')
 plt.hist(data[:1e4, 2], normed=True, range=(0, 1))
-plt.title('LCG 3')
 
-plt.subplot(224)
-plt.hist(data[:1e4, 3], normed=True, range=(0, 1))
-plt.title('LCG 4')
+ax = fig.add_subplot(224, title='LCG4')
+plt.hist(data[:, 3], normed=True, range=(0, 1))
 
-plt.savefig("lcg_hist.pdf")
+plt.savefig('lcg_hist.png', dpi=200)
 
-plt.figure()
-plt.subplot(221)
-plt.plot(data[:6074:2, 0], data[1:6075:2, 0], 'b,')
-plt.title('LCG1')
+fig = plt.figure()
+ax = fig.add_subplot(221, title='LCG1')
+ax.plot(data[::2, 0], data[1::2, 0], 'r,')
 
-plt.subplot(222)
-plt.plot(data[:256:2, 1], data[1:256:2, 1], 'b,')
-plt.title('LCG2')
+ax = fig.add_subplot(222, title='LCG2')
+ax.plot(data[::2, 1], data[1::2, 1], 'r,')
 
-plt.subplot(223)
-plt.plot(data[::2, 2], data[1::2, 2], 'b,')
-plt.title('LCG3')
+ax = fig.add_subplot(223, title='LCG3')
+ax.plot(data[::2, 2], data[1::2, 2], 'r,')
 
-plt.subplot(224)
-plt.plot(data[::2, 3], data[1::2, 3], 'b,')
-plt.title('LCG4')
+ax = fig.add_subplot(224, title='LCG4')
+ax.plot(data[::2, 3], data[1::2, 3], 'r,')
 
-plt.savefig('lcg_corr.png')
+plt.savefig('lcg_corr.png', dpi=100)
+
+
+fig = plt.figure()
+
+x = data[:-1:3, :].T
+y = data[1::3, :].T
+z = data[2::3, :].T
+
+ax = fig.add_subplot(221, projection='3d', title='LCG1')
+ax.scatter(y[0], z[0], x[0], c='r', marker='.', alpha=0.1)
+
+ax = fig.add_subplot(222, projection='3d', title='LCG2')
+ax.scatter(x[1], y[1], z[1], c='r', marker='.', alpha=0.1)
+
+ax = fig.add_subplot(223, projection='3d', title='LCG3')
+ax.scatter(y[2], z[2], x[2], c='r', marker='.', alpha=0.1)
+
+ax = fig.add_subplot(224, projection='3d', title='LCG4')
+ax.scatter(y[3], z[3], x[3], c='r', marker='.', alpha=0.1)
+
+fig.savefig('lcg_corr_3.png', dpi=900)
--- src/08/rand.cpp
+++ src/08/rand.cpp
@@ -1,63 +1,58 @@
-#include <assert.h>
 #include <cmath>
 #include <fstream>
 #include <functional>
-#include <iostream>
 #include <tuple>
 
-int main()
-{
-	auto lcg_init = []( int64_t r0, int64_t a0, int64_t c0, int64_t m0){
-		return [=]() mutable{
-			r0 = (a0 * r0 + c0) % m0;
-			return static_cast<double>(r0) / m0;
-		};
-	};
-
-	auto lcg1 = lcg_init(123456789, 65539, 0, 2147483648 );
-	auto lcg2 = lcg_init(123456789, 65539, 0, 2147483648 );
-	auto lcg3 = lcg_init(123456789, 65539, 0, 2147483648 );
-	auto lcg4 = lcg_init(123456789, 65539, 0, 2147483648 );
-
-	auto bm = [&](){
-		auto x1 = lcg1();
-		auto x2 = lcg1();
-		auto y1 = sqrt(-2 * log(x1)) * cos(2 * M_PI * x2);
-		auto y2 = sqrt(-2 * log(x1)) * sin(2 * M_PI * x2);
-		return std::make_tuple(y1,y2);
-	};
-
-	std::function<double(unsigned int)> zgs;
-    zgs = [&](unsigned int n){
-	    assert(n!=0);
-        if (n == 1){
-            return (lcg2() - 0.5) / sqrt(static_cast<double>(n)/1);
-        }else
-            return (lcg2() - 0.5) / sqrt(static_cast<double>(n)/1) + zgs(n-1);
+int main() {
+    auto lcg_init = [](int64_t r0, int64_t a0, int64_t c0, int64_t m0) {
+        return [=]() mutable {
+            r0 = (a0 * r0 + c0) % m0;
+            return r0 * 1. / m0;
+        };
     };
 
-    auto inv = [&](){return pow(lcg3(),1.0/3);};
+    auto lcg1 = lcg_init(1234, 16807, 0, 2147483647);
+    auto lcg2 = lcg_init(1234, 16807, 0, 2147483647);
+    auto lcg3 = lcg_init(1234, 16807, 0, 2147483647);
+    auto lcg4 = lcg_init(1234, 16807, 0, 2147483647);
 
-	std::function<double()> rueck;
-    rueck = [&](){
-		auto x1 = lcg4();
-		auto x2 = lcg4();
-		if (x2*M_PI<(sin(x1*M_PI)/2)) return M_PI * x1;
-		else return rueck();
+    auto bm = [&lcg1]() {
+        double x1 = lcg1();
+        double x2 = lcg1();
+        double y1 = sqrt(-2 * log(x1)) * cos(2 * M_PI * x2);
+        double y2 = sqrt(-2 * log(x1)) * sin(2 * M_PI * x2);
+        return std::make_tuple(y1, y2);
     };
 
+    auto zgs = [&lcg2](unsigned int n) {
+        double sum = 0;
+        for (unsigned int i = 0; i < n; ++i) sum += (lcg2() - 0.5);
+        return sum / sqrt(n * 1. / 12);
+    };
 
+    auto inv = [&lcg3]() { return pow(lcg3(), 1.0 / 3); };
 
-	double x2,x1;
-	std::ofstream file("rand.txt");
-	// file << "#bm\tlcg2\tlcg3\tlcg4" << std::endl;
-	for (int i=0;i<1e5;i++){
-		std::tie(x1,x2) = bm();
-		file << x1 << "\t" << zgs(5000) << "\t" << rueck() << "\t" << inv() << std::endl;
-		file << x2 << "\t" << zgs(5000) << "\t" << rueck() << "\t" << inv() << std::endl;
-	}
-	file.close();
+    std::function<double()> rueck;
+    rueck = [&lcg4, &rueck]() {
+        auto x1 = lcg4();
+        auto x2 = lcg4();
+        if (x2 * M_PI < (sin(x1 * M_PI) / 2))
+            return M_PI * x1;
+        else
+            return rueck();
+    };
 
+    double x2, x1;
 
-	return 0;
+    std::ofstream file("rand.txt");
+    file << "#bm\tzgs\trueck\tinv" << std::endl;
+
+    for (int i = 0; i < 100000; i++) {
+        std::tie(x1, x2) = bm();
+        file << x1 << "\t" << zgs(5000) << "\t" << rueck() << "\t" << inv() << std::endl;
+        file << x2 << "\t" << zgs(5000) << "\t" << rueck() << "\t" << inv() << std::endl;
+    }
+    file.close();
+
+    return 0;
 }
