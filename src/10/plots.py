b0316b329848b0c819bffe39178d88e4105be79b
hassec <hasse.christoph@outlook.de>
2015-06-26 23:05:55 +0200

    plots finally look decent. changed output on rndwalk.cpp.

--- src/10/plots.py
+++ src/10/plots.py
@@ -10,10 +10,6 @@ plt.style.use('ggplot')
 # ––– 1 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 
-def P(x, y, t):
-    return 1 / (4*0.1789810 * np.pi * t) * np.exp(-(x**2 + y**2) / (4*0.1789810 * t))
-
-
 def f(t, m, b):
     return 4 * m * t + b
 
@@ -21,54 +17,66 @@ def f(t, m, b):
 a = [1]
 for a in a:
     data = np.genfromtxt('rnd_a_' + str(a) + '.txt')
-    N = len(data[0, 0::2])
-    t = np.linspace(1, len(data[:, 0]) + 1, len(data[:, 0]))
-    rx = data[:, ::2]
-    ry = data[:, 1::2]
-    rxm = np.sum(rx, axis=1) / N
-    rym = np.sum(ry, axis=1) / N
-    r2 = np.sum((rx**2 + ry**2), axis=1) / N
+    t = np.linspace(1, len(data[:, 0]), len(data[:, 0]))
+    rxm = data[:,0]
+    rym= data[:,1]
+    r2 = data[:,2]
     plt.figure()
     plt.plot(t, r2 / (a**2), 'r,')
+    plt.xlabel(r'$\langle r(t)^2\rangle/a^2$')
+    plt.ylabel(r't/$\tau$')
     plt.savefig("r2_a_" + str(a) + ".png")
     plt.figure()
     plt.plot(rxm / a, rym / a, 'r,', alpha=0.7)
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.savefig("r_a_" + str(a) + ".png")
 
     par, _ = opt.curve_fit(f, t, r2)
+    if (a==1):
+        D = par[0]
     plt.figure()
-    plt.plot(t, f(t, par[0], par[1]), '-k')
-    plt.plot(t, r2, 'r,')
+    plt.plot(t, f(t, par[0], par[1]), '--k',label="Ausgleichsgerade")
+    plt.plot(t, r2, 'r,',label="Datenpunkte")
+    plt.xlabel(r'$\langle r(t)^2\rangle$')
+    plt.ylabel(r't/$\tau$')
     plt.title("D = " + str(par[0]))
+    plt.legend(loc='best')
     plt.savefig("r2_fit_" + str(a) + ".png")
 
-    ranges = [25, 35, 50]
-    ti = [9, 99, 999]
-    for i in range(0, 3):
-        if (a == 1):
-            x = data[ti[i], ::2]
-            y = data[ti[i], 1::2]
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
+def P(x, y, t):
+    return 1 / (4*D * np.pi * t) * np.exp(-(x**2 + y**2) / (4*D * t))
+ranges = [25, 35, 75]
+bins = [50,70,75]
+ti = [10, 100, 1000]
+data = np.genfromtxt("rnd_a_1_hist.txt")
+for i in range(0, 3):
+    if (a == 1):
+        x = data[i, ::2]
+        y = data[i, 1::2]
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
-            hist, xedges, yedges = np.histogram2d(x, y, range=[
-                                                  [-ranges[i], ranges[i]], [-ranges[i], ranges[i]]], bins=2 * ranges[i], normed=True)
+        hist, xedges, yedges = np.histogram2d(x, y, range=[
+                                              [-ranges[i], ranges[i]], [-ranges[i], ranges[i]]], bins=bins[i], normed=True)
 
-            elements = (len(xedges) - 1) * (len(yedges) - 1)
-            xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
+        elements = (len(xedges) - 1) * (len(yedges) - 1)
+        xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
 
-            xpos = xpos.flatten()
-            ypos = ypos.flatten()
-            zpos = np.zeros(elements)
-            dx = 0.5 * np.ones_like(zpos)
-            dy = dx.copy()
-            dz = hist.flatten()
+        xpos = xpos.flatten()
+        ypos = ypos.flatten()
+        zpos = np.zeros(elements)
+        dx = 0.5 * np.ones_like(zpos)
+        dy = dx.copy()
+        dz = hist.flatten()
 
-            ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='r',
-                     zsort='average')
-            xy = np.arange(-ranges[i], ranges[i], 0.1)
-            ax.plot(xy, xy, P(xy, xy, ti[i]+1), 'r-')
-            plt.savefig("hist" + str(ti[i]+1) + ".png")
+        ax.bar3d(xpos, ypos, zpos, dx, dy, dz,
+                 zsort='max')
+        xy = np.arange(-ranges[i], ranges[i], 0.1)
+        ax.plot(xy, xy, P(xy, xy, ti[i]), 'r-')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.savefig("hist" + str(ti[i]) + ".png")
 
 
 # ––– 2 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
--- src/10/rndwalk.cpp
+++ src/10/rndwalk.cpp
@@ -4,30 +4,50 @@
 #include <vector>
 
 
-void rnd_walk(double a, size_t n_walks, size_t steps, std::string filename){
+void rnd_walk(double a, size_t n_walks, size_t steps){
 
   std::mt19937 gen;
   std::uniform_real_distribution<double> dist(-a, std::nextafter(a, a + 1));
 
   auto rnd = [&dist, &gen]() { return dist(gen); };
 
-  std::vector<std::vector<double>> data(2, std::vector<double>(steps, 0));
+  std::vector<std::vector<double>> data(2, std::vector<double>(n_walks, 0));
   
-  FILE* fout = fopen(filename.c_str(), "w");
-  fprintf(fout, "#t\trx1\try1\t....\n");
+  std::stringstream f1("");
+  std::stringstream f2("");
+  f1 << "rnd_a_" << a << ".txt";
+  f2 << "rnd_a_" << a << "_hist.txt";
 
-  for (size_t i = 1; i <= n_walks; i++) {
+
+  FILE* fout1 = fopen(f1.str().c_str(), "w");
+  FILE* fout2 = fopen(f2.str().c_str(), "w");
+  fprintf(fout1, "#t\trx1\try1\tr2\n");
+
+  for (size_t i = 1; i <= steps; i++) {
     // #pragma omp parallel for
-    for (size_t j = 0; j < steps; j++) {
+    for (size_t j = 0; j < n_walks; j++) {
       data[0][j] += rnd();
       data[1][j] += rnd();
     }
+    if (a == 1.0 && (i == 10 or i == 100 or i == 1000)){
       for(auto it1 = data[0].begin(),it2=data[1].begin(); it1 != data[0].end();it1++,it2++){
-      fprintf(fout, "%f\t%f\t", *it1, *it2);
+        fprintf(fout2, "%f\t%f\t", *it1, *it2);
       }
-      fprintf(fout, "\n");
+      fprintf(fout2, "\n");
+    }
+
+    double rx = std::accumulate(data[0].begin(), data[0].end(), 0.0) / n_walks;
+    double ry = std::accumulate(data[1].begin(), data[1].end(), 0.0) / n_walks ;
+    double r2 = std::inner_product(data[0].begin(), data[0].end(),
+                                     data[0].begin(), 0.0) / n_walks +
+                  std::inner_product(data[1].begin(), data[1].end(),
+                                     data[1].begin(), 0.0) / n_walks;
+
+    fprintf(fout1, "%f\t%f\t%f\n", rx, ry,r2);     
+
   }
-  fclose(fout);
+  fclose(fout1);
+  fclose(fout2);
 
 
 }
@@ -35,13 +55,11 @@ void rnd_walk(double a, size_t n_walks, size_t steps, std::string filename){
 
 int main() {
   const double a[4] = {1.0, 1.0e-1, 1.0e-2  , 10.0};
-  const size_t n_walks = 1000;
+  const size_t n_walks = 100000;
   const size_t steps = 1000;
 
   for (int i = 0; i < 1; i++){
-  std::stringstream filename("");
-  filename << "rnd_a_" << a[i] << ".txt";
-  rnd_walk(a[i],  n_walks, steps, filename.str());
+  rnd_walk(a[i],  n_walks, steps);
   }
   return 0;
 }
